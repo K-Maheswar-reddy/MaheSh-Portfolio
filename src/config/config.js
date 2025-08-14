69b544495fb55eaa6276440bca0eb0b1d9b757f5
K-Maheswar-reddy <maheshreddy77570k@gmail.com>
2025-08-14 06:32:36 +0530

    Update backend server URL

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    "SERVER_URL": "https://mern-portfolio-backend.vercel.app/api", //backend url
+    "SERVER_URL": "https://mahesh-portfolio-nwfc.vercel.app/", //backend url
     "THEME_COLORS":{
         PRIMARY: "#007bff",
         SECONDARY: "#fc3"
