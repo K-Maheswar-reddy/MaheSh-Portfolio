bf35b1ddff168f07dd55490047a31a7afb08782e
K-Maheswar-reddy <maheshreddy77570k@gmail.com>
2025-08-14 06:54:26 +0530

    Update front end server URL

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    "SERVER_URL": "https://mahesh-portfolio-nwfc.vercel.app/", //backend url
+    "SERVER_URL": "https://mahesh-portfolio-nwfc-5moxmey8r-mahesh-ks-projects-7f6cbb91.vercel.app/", //backend url
     "THEME_COLORS":{
         PRIMARY: "#007bff",
         SECONDARY: "#fc3"
